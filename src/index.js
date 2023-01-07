fb90e5adc6dd6b77d5007dd0ef3a0c1bd1ddea13
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-07 12:17:39 -0500

    Add initial config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from './app'
 
 const container = document.getElementById('app')
 const root = ReactDom.createRoot(container)
-root.render(<h1>Hola platzi</h1>)
+root.render(<App />)
